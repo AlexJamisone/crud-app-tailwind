e0e2236477a72d7bceeab28eab8a2e84178d06a0
AlexJamisone <business.homeit@gmail.com>
2022-11-24 18:35:09 +0300

    Finish Create Handler function to create Note

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,59 @@ import Image from 'next/image';
 import { format } from 'path';
 import { useState } from 'react';
 
-
 interface FormData {
-	title: string
-	content: string
-	id: string
+	title: string;
+	content: string;
+	id: string;
 }
 
 const Home: NextPage = () => {
-	const [form, setForm] = useState<FormData>({title: '', content: '', id: ''})
+	const [form, setForm] = useState<FormData>({
+		title: '',
+		content: '',
+		id: '',
+	});
+
+	//Helper function
+
+	const clearForm = () => {
+		setForm({ title: '', content: '', id: '' });
+	};
+
+	//CRUD funcrion
+
+	const create = async (data: FormData) => {
+		try {
+			fetch('http://localhost:3000/api/create', {
+				body: JSON.stringify(data),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				method: 'POST',
+			});
+			clearForm();
+		} catch (error) {
+			console.log('Falure to create Note', error);
+		}
+	};
+
+	//Custom Handler
+
+	const handlerSubmit = async (data: FormData) => {
+		try {
+			create(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="mt-10">
 			<h1 className="text-center font-bold text-2xl mt-4">Notes</h1>
 			<form
 				onSubmit={(e) => {
 					e.preventDefault();
+					handlerSubmit(form);
 				}}
 				className="w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch"
 			>
@@ -26,16 +64,25 @@ const Home: NextPage = () => {
 					type="text"
 					placeholder="Title"
 					value={form.title}
-					onChange={e => setForm({...form, title: e.target.value})}
-					className='border-2 rounded border-gray-600 p-1'
+					onChange={(e) =>
+						setForm({ ...form, title: e.target.value })
+					}
+					className="border-2 rounded border-gray-600 p-1"
 				/>
 				<textarea
 					placeholder="Content"
 					value={form.content}
-					onChange={e => setForm({...form, content: e.target.value})}
-					className='border-2 rounded border-gray-600 p-1'
+					onChange={(e) =>
+						setForm({ ...form, content: e.target.value })
+					}
+					className="border-2 rounded border-gray-600 p-1"
 				/>
-				<button type='submit' className='bg-blue-500 text-white rounded p-1'>Add +</button>
+				<button
+					type="submit"
+					className="bg-blue-500 text-white rounded p-1"
+				>
+					Add +
+				</button>
 			</form>
 		</div>
 	);
